d0f226713e4f62ba8f5e059cd0b339a521e1ea5a
Vedant1306 <101728197+Vedant1306@users.noreply.github.com>
2022-03-17 18:01:10 +0530

    Add files via upload

---
+++ math.py
@@ -0,0 +1,15 @@
+#Add implementation
+def add(x,y);
+	pass
+
+#Subtract Implementation
+def sub(x,y);
+	pass
+
+#Multiply Impementation
+def Multiply(x,y);
+	pass
+
+#Divide Implementation
+def Divide(x,y);
+	pass
